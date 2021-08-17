569bcf18cc22bf7c8c52c618eb393d0ccc411ad1
Eidmone <eidmonetagaca@gmail.com>
2021-08-16 20:30:24 -0700

    Updates

---
+++ precache-manifest.1e3fbbadf8c75e9b968e18280846942e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84790cdb59f39f00a24f77456e4429eb",
+    "url": "/myspend/index.html"
+  },
+  {
+    "revision": "ff482c7f5af5c8052ab2",
+    "url": "/myspend/static/css/main.59440c72.chunk.css"
+  },
+  {
+    "revision": "ce76de47f9da92a47d0e",
+    "url": "/myspend/static/js/2.6f6874dc.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/myspend/static/js/2.6f6874dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff482c7f5af5c8052ab2",
+    "url": "/myspend/static/js/main.d176ba4b.chunk.js"
+  },
+  {
+    "revision": "3f540f0b7c7d1c898c10",
+    "url": "/myspend/static/js/runtime-main.8c7da71d.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/myspend/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/myspend/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/myspend/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/myspend/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/myspend/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/myspend/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/myspend/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/myspend/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/myspend/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/myspend/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "aabc3fc1441264a5c30f674a8217620b",
+    "url": "/myspend/static/media/question1.aabc3fc1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Microsoft2021/precache-manifest.fa8e664e552eab821e5aa683aa23b09b.js"
+  "/myspend/precache-manifest.1e3fbbadf8c75e9b968e18280846942e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Microsoft2021/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myspend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
