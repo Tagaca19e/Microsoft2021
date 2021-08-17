ef5d6558acdd67237110fee7ebe53f95375adef7
Eidmone <eidmonetagaca@gmail.com>
2021-08-16 20:13:58 -0700

    changed icons

--- .history/src/components/sections/FeaturesTiles_20210816201250.js
+++ .history/src/components/sections/FeaturesTiles_20210816201250.js
@@ -182,7 +182,11 @@ const FeaturesTiles = ({
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">#Leader</h4>
-                  <p className="m-0 text-sm"></p>
+                  <p className="m-0 text-sm">
+                    {/* Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat. */}
+                  </p>
                 </div>
               </div>
             </div>
